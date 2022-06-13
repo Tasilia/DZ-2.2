85526a986e1903589f475285edb476210aff53e4
Tasilia <tsygnas@gmail.com>
2022-06-13 11:34:59 +0300

    Ver 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         int balance = 100; // баланс телефона
         int refill = 1310; // сумма пополнения счёта
-        int bonus = (refill > 1000) ? refill/100 : 0; // бонусные рубли
+        int bonus = (refill > 1000) ? refill / 100 : 0; // бонусные рубли
         int total = balance + refill + bonus;
         System.out.println("Итоговый счёт: " + total);
         System.out.println("Количество бонусных рублей: " + bonus);
